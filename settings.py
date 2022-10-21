ff3fac343d2710293a6d6b497bc6a93c0384a4d9
treborbly <robertsimonbailey@gmail.com>
2022-10-22 01:16:16 +0800

    Create settings.py

---
+++ settings.py
@@ -0,0 +1,7 @@
+
+
+def dashboard_settings():
+    print('The settings panel is under maintenance...')
+    print('Please try again later.')
+    print('For more questions, please contact the developer.')
+
