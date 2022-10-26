ff341c4906f3f09a5c7d6e0cdfc3e0ec488f9136
treborbly <robertsimonbailey@gmail.com>
2022-10-26 11:52:40 +0800

    Delete settings.py

--- settings.py
+++
@@ -1,7 +0,0 @@
-
-
-def dashboard_settings():
-    print('The settings panel is under maintenance...')
-    print('Please try again later.')
-    print('For more questions, please contact the developer.')
-
