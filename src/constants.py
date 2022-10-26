5b345cb8a8d4350c41ba399f514238a4926e4f8e
treborbly <robertsimonbailey@gmail.com>
2022-10-26 11:52:37 +0800

    Create constants.py

---
+++ src/constants.py
@@ -0,0 +1,11 @@
+
+
+LOGIN = "/Users/robertbailey/Documents/GitHub/finance-dashboard/ui/login.ui"
+
+DASHBOARD = "/Users/robertbailey/Documents/GitHub/finance-dashboard/ui/dashboard.ui"
+
+SAVINGS = "/Users/robertbailey/Documents/GitHub/finance-dashboard/ui/savings.ui"
+
+EXPENDABLES = "/Users/robertbailey/Documents/GitHub/finance-dashboard/ui/expendables.ui"
+
+PHONE = "/Users/robertbailey/Documents/GitHub/finance-dashboard/ui/phone.ui"
